60f44df09be493b0c1c1b73558b40b2a042f4bd8
aryoyudhanto <aryoyudhanto26399@gmail.com>
2022-12-12 11:43:38 +0700

    push kerangka project, clone dan npm install di folder masing2

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+})
