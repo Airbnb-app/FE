2290c9bb090dac9cd4542271c7b0f6231fbd7616
aryoyudhanto <aryoyudhanto26399@gmail.com>
2022-12-13 11:48:37 +0700

    fixed layout, dari sidebar, container, layout

--- component/Container.jsx
+++ component/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({children}) => {
   return (
-    <div className='flex w-full h-full bg-white'>{children}</div>
+    <div className='flex w-full h-full bg-cream-airbnb'>{children}</div>
   )
 }
 
