439ca2333d4616d1520f6450d44c140c257f9e36
aryoyudhanto <aryoyudhanto26399@gmail.com>
2022-12-16 00:12:29 +0700

    integrasi reservation to payment

---
+++ pages/payment/[titleId].jsx
@@ -0,0 +1,189 @@
+import React, { useState, useEffect } from 'react'
+import Layout from '../../components/Layout'
+import Navbar from '../../components/Navbar'
+import { useCookies } from "react-cookie"
+import Swal from 'sweetalert2'
+import Router, { useRouter } from 'next/router'
+import axios from 'axios'
+
+function Payment() {
+  const router = useRouter()
+  const [cookie, setCookie, removeCookie] = useCookies()
+  const [name, setName] = useState()
+  const [fullName, setFullName] = useState()
+  const [cardNumber, setCardNumber] = useState()
+  const [cvv, setCvv] = useState()
+  const [exMonth, setExMonth] = useState()
+  const [exYear, setExYear] = useState()
+
+
+  const title = router?.query?.titleHomestay
+  const durasi = router?.query?.duration
+  const idHome = router?.query?.id_homestay
+  const price = router?.query?.price_per_night
+  const totalPrice = router?.query?.total_price
+  const start = router?.query?.start_date
+  const end = router?.query?.end_date
+  const user_id = cookie.user_id
+
+  console.log("cek title: ", router?.query?.titleHomestay)
+  console.log("cek durasi: ", router?.query?.duration)
+  console.log("cek id homestay: ", router?.query?.id_homestay)
+  console.log("cek price/night: ", router?.query?.price_per_night)
+  console.log("cek total price: ", router?.query?.total_price)
+  console.log("cek start: ", router?.query?.start_date)
+  console.log("cek end: ", router?.query?.end_date)
+
+  const confirmHandler = (e) =>{
+    e.preventDefault()
+    axios.post(`https://numpangtidur.my.id/reservations`,
+   { homestay_id: parseInt(idHome),
+    start_date: start,
+    duration: parseInt(durasi),
+    total_price: parseInt(totalPrice),
+    user_id: parseInt(user_id),
+    payments: {
+      credit_card: "visa",
+      name: fullName,
+      card_number: cardNumber,
+      cvv: cvv,
+      month: exMonth,
+      year: exYear,
+    },
+  }, {
+    headers: {
+      Authorization: `Bearer ${cookie.token}`
+    },
+  }
+    ).then((response)=>{
+      console.log("jadi: ",response)
+    }).catch((err)=>{
+      console.log("gagal: ",err)
+    })
+  }
+  console.log("user id", user_id)
+  console.log("cek fullname: ", fullName)
+  console.log("cek card number: ", cardNumber)
+  console.log("cek cvv: ", cvv)
+  console.log("cek ex month: ", exMonth)
+  console.log("cek ex year: ", exYear)
+
+  useEffect(() => {
+    setName(cookie.name);
+  }, [])
+
+  const logoutHandler = () => {
+    Swal.fire({
+      title: "Are you sure want to logout?",
+      // text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: "Yes",
+      cancelButtonColor: "#d33",
+      cancelButtonText: "No",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          text: "Logout successfully",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        removeCookie("name");
+        removeCookie("role");
+        removeCookie("token");
+        removeCookie("user_id");
+        Router.push('/');
+      }
+    })
+
+    useEffect(() => {
+      if (!cookie.token) {
+        Router.push("/");
+      }
+    }, [cookie.token]);
+
+  }
+
+  useEffect(() => {
+    if (!cookie.token) {
+      Router.push("/");
+    }
+  }, [cookie.token]);
+
+  return (
+    <Layout
+      logout={() => logoutHandler()}
+      name={
+        name === undefined ? (
+          <><p>you are not login</p></>
+        ) : (
+          <><p className='mt-2'>{name}</p></>
+        )
+      }>
+      <Navbar namePages="Payment" />
+      <div className='w-[90%] bg-[#FBFBFB] flex flex-col items-center justify-center rounded-lg py-6 text-black font-[Poppins]'>
+        <form class="w-[90%] " onSubmit={(e)=>confirmHandler(e)}>
+          <p className='text-3xl text-pink-airbnb font-bold py-5 '>Credit Card</p>
+          <div class="flex flex-wrap -mx-3 mb-5">
+            <div class="w-full px-3">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-pink-airbnb" id="grid-password" type="text" placeholder="Cardholder name" 
+              onChange={(e)=>{setFullName(e.target.value)}}/>
+            </div>
+          </div>
+          <div class="flex flex-wrap -mx-3 mb-5">
+            <div class="w-full md:w-2/3 px-3 mb-6 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border  py-3 px-4 mb-3 leading-tight rounded focus:outline-none focus:bg-white focus:border-pink-airbnb" id="grid-first-name" type="text" placeholder="Card Number" 
+              onChange={(e)=>{setCardNumber(e.target.value)}}/>
+            </div>
+            <div class="w-full md:w-1/3 px-3">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-pink-airbnb" id="grid-last-name" type="text" placeholder="CVV" 
+              onChange={(e)=>{setCvv(e.target.value)}}/>
+            </div>
+          </div>
+          <div class="flex flex-wrap -mx-3 mb-3">
+            <div class="w-full md:w-1/3 px-3 mb-3 md:mb-0 flex items-center justify-center">
+              <p className='text-black'>Expiration Date</p>
+            </div>
+            <div class="w-full md:w-1/3 px-3 mb-3 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-pink-airbnb" id="grid-last-name" type="text" placeholder="MM" 
+              onChange={(e)=>{setExMonth(e.target.value)}}/>
+            </div>
+            <div class="w-full md:w-1/3 px-3 mb-3 md:mb-0">
+              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-pink-airbnb" id="grid-last-name" type="text" placeholder="YYYY" 
+              onChange={(e)=>{setExYear(e.target.value)}}/>
+            </div>
+          </div>
+          <div className='w-full bg-[#FBFBFB] flex items-center justify-center rounded-lg pb-6 text-black'>
+            <div className='grid grid-col w-[90%] pt-5'>
+              <p className='text-3xl text-pink-airbnb font-bold py-3'>{router?.query?.titleHomestay}</p>
+              <div className='flex justify-between pt-3'>
+                <div className='flex flex-row gap-3'>
+                  <div className='mr-5'>
+                    <p>check in</p>
+                    <p className='text-xl font-semibold'>{router?.query?.start_date}</p>
+                  </div>
+                  <div className='ml-5'>
+                    <p className=' '>check out</p>
+                    <p className=' text-xl font-semibold'>{router?.query?.end_date}</p>
+                  </div>
+                </div>
+                <div className='text-xl font-semibold'>
+                  <p>{router?.query?.duration} nights @ {router?.query?.price_per_night}/night</p>
+                  <p className='text-end'>Total ${router?.query?.total_price}</p>
+                </div>
+              </div>
+              <div className='flex justify-end pt-5'>
+                <button className="btn h-12 border-none bg-[#FF5A60] w-20 text-white" type='submit'>Confirm</button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </Layout >
+  )
+}
+
+export default Payment
--- pages/reservation/[title].jsx
+++ pages/reservation/[title].jsx
@@ -13,30 +13,43 @@ const Homestay = () => {
   const router = useRouter()
   const [cookie, setCookie, removeCookie] = useCookies();
   const [avail, setAvail] = useState()
-  const [homestay_id, setHomestayId] = useState(router?.query?.id)
+  const [homestay_id, setHomestayId] = useState()
   const [start_date, setStartDate] = useState()
   const [end_date, setEndDate] = useState()
   const [loading, setLoading] = useState(false);
 
-  console.log("cek router address: ", router?.query?.address)
-  console.log("cek router owner: ", router?.query?.owner)
-  console.log("cek router image: ", router?.query?.image1)
-  console.log("cek router image: ", router?.query?.image2)
-  console.log("cek router image: ", router?.query?.image3)
-  console.log("cek router nama: ", router?.query?.name)
-  console.log("cek router desk: ", router?.query?.description)
-  console.log("cek router harga: ", router?.query?.price_per_night)
-  console.log("cek router id: ", router?.query?.id)
+  //data berhasil cek
+  const [duration, setDuration] = useState()
+  const [idHomestay, setIdHomestay] = useState()
+  const [pricePerNight, setPricePerNight] = useState()
+  const [totalPrice, setTotalPrice] = useState()
+  const titleHomestay = router?.query?.name
+  console.log("cekk nama home:", titleHomestay)
+
+
+  //untuk data ke payment
+  // const homestayId = router?.query?.id
+  // const 
+
+  // console.log("cek router address: ", router?.query?.address)
+  // console.log("cek router owner: ", router?.query?.owner)
+  // console.log("cek router image: ", router?.query?.image1)
+  // console.log("cek router image: ", router?.query?.image2)
+  // console.log("cek router image: ", router?.query?.image3)
+  // console.log("cek router nama: ", router?.query?.name)
+  // console.log("cek router desk: ", router?.query?.description)
+  // console.log("cek router harga: ", router?.query?.price_per_night)
+  // console.log("cek router id: ", router?.query?.id)
 
   console.log("tanggal mulai", start_date)
   console.log("tanggal akhir", end_date)
 
+  const id = router?.query?.id
+
   const checkAvail = (e) => {
     e.preventDefault();
-    // setHomestayId(router?.query?.id)
-    
     axios.post(`https://numpangtidur.my.id/reservations/check`, {
-      homestay_id: homestay_id,
+      homestay_id: parseInt(id),
       start_date: start_date,
       end_date: end_date,
     }, {
@@ -45,14 +58,32 @@ const Homestay = () => {
       }
     }
     ).then((response) => {
-      console.log("berhasil check: ", response)
+      console.log("berhasil check: ", response.data.data)
+      setHomestayId(response.data.data)
+      const { duration, homestay_id, price_per_night, total_price } = response.data.data
+      setDuration(duration);
+      setIdHomestay(homestay_id)
+      setPricePerNight(price_per_night)
+      setTotalPrice(total_price)
     }).catch((error) => {
       console.log("err:", error)
     })
   }
-  // useEffect(() => {
-  //   setHomestayId(router?.query?.id);
-  // }, [])
+
+  const reserve = (titleId) => {
+    Router.push({
+      pathname: `/payment/${titleId}`,
+      query: {
+        titleHomestay: titleHomestay,
+        duration: duration,
+        id_homestay: idHomestay,
+        price_per_night: pricePerNight,
+        total_price: totalPrice,
+        start_date: start_date,
+        end_date: end_date,
+      }
+    })
+  }
 
   const logoutHandler = () => {
     Swal.fire({
@@ -82,7 +113,9 @@ const Homestay = () => {
     })
 
   }
-
+  // useEffect(()=>{
+  //   checkAvail()
+  // }, [])
   return (
     <Layout dashboard={"shadow"} logout={() => logoutHandler()}>
       <Navbar namePages={"Reservation "} />
@@ -101,7 +134,8 @@ const Homestay = () => {
       <div className="w-full flex justify-end">
         <div className='w-1/4 h-full bg-[#FBFBFB] rounded-lg shadow-xl mx-20'>
           <div className='m-5'>
-            <form className='' onSubmit={(e) => checkAvail(e)}>
+            <form className=''
+              onSubmit={(e) => checkAvail(e)}>
               <div className='my-2'>
                 <Input variant="outlined" size="lg" label="Check-in Date" type="text"
                   onChange={(e) => { setStartDate(e.target.value) }}
@@ -113,9 +147,46 @@ const Homestay = () => {
                   value={end_date} />
               </div>
               <div className='flex justify-center my-7'>
-                <button type='submit' className='btn bg-pink-airbnb border-none hover:bg-[#E75056] normal-case text-[22px]' >Check Availability</button>
+                {
+                  homestay_id ?
+                    (
+                      <>
+                        <button 
+                        className='btn bg-pink-airbnb border-none hover:bg-[#E75056] normal-case text-[22px]'
+                        onClick={()=>reserve(idHomestay)}
+                        >
+                          Reserve</button>
+                      </>
+                    ) : (
+                      <>
+                        <button type='submit' className='btn bg-pink-airbnb border-none hover:bg-[#E75056] normal-case text-[22px]' >
+                          Check Availability</button>
+                      </>
+                    )
+                }
               </div>
             </form>
+            <div>
+              {
+                homestay_id ?
+                  (
+                    <>
+                    <div className='text-black'>
+                      <p>Duration: {duration}</p>
+                      <p>ID Homestay: {idHomestay}</p>
+                      <p>Price/Night: {pricePerNight}</p>
+                      <p>Total: {totalPrice}</p>
+                    </div>
+                    </>
+                  ) : (
+                    <>
+                    <div className='text-black'>
+                      <p>check again</p>
+                    </div>
+                    </>
+                  )
+              }
+            </div>
           </div>
         </div>
       </div>
